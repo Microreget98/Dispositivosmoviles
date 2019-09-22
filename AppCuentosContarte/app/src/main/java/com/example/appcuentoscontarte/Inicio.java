e7e476d6ff80db7635a18083f4bdc2e8ad181cd2
AleScods <alejandramonze@hotmail.com>
2019-09-22 12:02:11 -0500

    Portadas terminadas y audios incluidos

--- AppCuentosContarte/app/src/main/java/com/example/appcuentoscontarte/Inicio.java
+++ AppCuentosContarte/app/src/main/java/com/example/appcuentoscontarte/Inicio.java
@@ -1,5 +1,6 @@
 package com.example.appcuentoscontarte;
 
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,9 +18,9 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener{
 
 
     ImageButton negro, cafe, naranja,rojo, rosa, morado, amarillo,verdelimon, verde,turquesa, celeste, azul;
-    MediaPlayer mp,mp1,mp2,mp3,mp4,mp5,mp6;
-    Button btnreproducir;
-    Button btnsiguiente;
+    MediaPlayer mp,mp1,mp2,mp3,mp4,mp5,mp6,mp7,mp8,mp9,mp10,mp11,mp12,mp13,mp14,mp15,mp16,mp17,mp18,mp19,mp20,mp21,mp22,mp23;
+    ImageButton btnreproducir;
+    ImageButton btnsiguiente;
     TextView tvCuento;
     private int current_frase, current_audio;
 
@@ -50,8 +51,8 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener{
         setSupportActionBar(toolbar); */
 
 
-        btnreproducir = (Button)findViewById(R.id.btnreproducir);
-        btnsiguiente = (Button)findViewById(R.id.btnsiguiente);
+        btnreproducir = (ImageButton) findViewById(R.id.btnreproducir);
+        btnsiguiente = (ImageButton)findViewById(R.id.btnsiguiente);
         tvCuento = (TextView) findViewById(R.id.tvCuento);
 
         negro = (ImageButton)findViewById(R.id.btnnegro);
@@ -101,7 +102,7 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener{
 
         if(cs.equals("caperucita")){ // Comparación de Cadenas con .equals
             Toast.makeText(getApplicationContext(), "La Caperucita Roja", Toast.LENGTH_LONG).show();
-            int[] sounds ={R.raw.caperucita1, R.raw.caperucita2, R.raw.caperucita3,R.raw.caperucita4,R.raw.caperucita5,R.raw.caperucita6,R.raw.caperucita7};
+            int[] sounds ={R.raw.caperucita1, R.raw.caperucita2, R.raw.caperucita3,R.raw.caperucita4,R.raw.caperucita5,R.raw.caperucita6,R.raw.caperucita7,R.raw.caperucita8,R.raw.caperucita9,R.raw.caperucita10,R.raw.caperucita11,R.raw.caperucita12,R.raw.caperucita13,R.raw.caperucita14,R.raw.caperucita15,R.raw.caperucita16,R.raw.caperucita17,R.raw.caperucita18,R.raw.caperucita19,R.raw.caperucita20,R.raw.caperucita21,R.raw.caperucita22,R.raw.caperucita23};
 
             mp = MediaPlayer.create(this, sounds[0]);
             mp1 = MediaPlayer.create(this, sounds[1]);
@@ -110,6 +111,23 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener{
             mp4 = MediaPlayer.create(this, sounds[4]);
             mp5 = MediaPlayer.create(this, sounds[5]);
             mp6 = MediaPlayer.create(this, sounds[6]);
+            mp7 = MediaPlayer.create(this, sounds[7]);
+            mp8 = MediaPlayer.create(this, sounds[8]);
+            mp9 = MediaPlayer.create(this, sounds[9]);
+            mp10 = MediaPlayer.create(this, sounds[10]);
+            mp11 = MediaPlayer.create(this, sounds[11]);
+            mp12 = MediaPlayer.create(this, sounds[12]);
+            mp13 = MediaPlayer.create(this, sounds[13]);
+            mp14 = MediaPlayer.create(this, sounds[14]);
+            mp15 = MediaPlayer.create(this, sounds[15]);
+            mp16 = MediaPlayer.create(this, sounds[16]);
+            mp17 = MediaPlayer.create(this, sounds[17]);
+            mp18 = MediaPlayer.create(this, sounds[18]);
+            mp19 = MediaPlayer.create(this, sounds[19]);
+            mp20 = MediaPlayer.create(this, sounds[20]);
+            mp21 = MediaPlayer.create(this, sounds[21]);
+            mp22 = MediaPlayer.create(this, sounds[22]);
+
 
             //mp.setOnCompletionListener(Inicio);
             mp.start();
@@ -151,54 +169,111 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener{
 
                     Toast.makeText(getApplicationContext(), "Se termino cuento", Toast.LENGTH_LONG).show();
                 }
-
                 current_audio++;
+
+                String cuenta= String.valueOf(current_audio);
+                String tamano = String.valueOf(frases.length);
+                Toast.makeText(getApplicationContext(), "Tamaño es "+tamano, Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "La cuenta es "+cuenta, Toast.LENGTH_LONG).show();
+
+
+               // Toast.makeText(getApplicationContext(), "La cuenta es "+cuenta, Toast.LENGTH_LONG).show();
                 //
                 //reproducirAudio();
                 switch (current_audio){
                     case 1:
                         mp.stop();
                         mp1.start();
-                        Toast.makeText(getApplicationContext(), "Se reproduce 1", Toast.LENGTH_LONG).show();
                         break;
                     case 2:
                         mp1.stop();
                         mp2.start();
-                        Toast.makeText(getApplicationContext(), "Se reproduce 2", Toast.LENGTH_LONG).show();
+                        break;
                     case 3:
                         mp2.stop();
                         mp3.start();
-                        Toast.makeText(getApplicationContext(), "Se reproduce 3", Toast.LENGTH_LONG).show();
+                        break;
                     case 4:
                         mp3.stop();
                         mp4.start();
+                        break;
                     case 5:
                         mp4.stop();
                         mp5.start();
+                        break;
                     case 6:
                         mp5.stop();
                         mp6.start();
+                        break;
+                    case 7:
+                        mp6.stop();
+                        mp7.start();
+                        break;
+                    case 8:
+                        mp7.stop();
+                        mp8.start();
+                        break;
+                    case 9:
+                        mp8.stop();
+                        mp9.start();
+                        break;
+                    case 10:
+                        mp9.stop();
+                        mp10.start();
+                        break;
+                    case 11:
+                        mp10.stop();
+                        mp11.start();
+                        break;
+                    case 12:
+                        mp11.stop();
+                        mp12.start();
+                        break;
+                    case 13:
+                        mp12.stop();
+                        mp13.start();
+                        break;
+                    case 14:
+                        mp13.stop();
+                        mp14.start();
+                        Toast.makeText(getApplicationContext(), "La cuenta es "+cuenta, Toast.LENGTH_LONG).show();
+                        break;
+                    case 15:
+                        mp14.stop();
+                        mp15.start();
+                        break;
+                    case 16:
+                        mp15.stop();
+                        mp16.start();
+                        break;
+                    case 17:
+                        mp16.stop();
+                        mp17.start();
+                        break;
+                    case 18:
+                        mp17.stop();
+                        mp18.start();
+                        break;
+                    case 19:
+                        mp18.stop();
+                        mp19.start();
+                        break;
+                    case 20:
+                        mp19.stop();
+                        mp20.start();
+                        break;
+                    case 21:
+                        mp20.stop();
+                        mp21.start();
+                        break;
+                    case 22:
+                        mp21.stop();
+                        mp22.start();
+                        break;
 
                 }
 
                 break;
-            case R.id.btnreproducir:
-                if(mp!=null){
-                    mp.start();
-                }
-
-                /*else if(mp.isPlaying()){
-                    mp.stop();
-                    mp.start();
-                }*/
-
-
-               /* ){
-
-                    mp.start();
-                }*/
-                break;
-
 
             case R.id.btnnegro:
                 color = v.getTag().toString();
--- AppCuentosContarte/app/src/main/java/com/example/appcuentoscontarte/Intermedio.java
+++ AppCuentosContarte/app/src/main/java/com/example/appcuentoscontarte/Intermedio.java
@@ -5,9 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Intermedio extends AppCompatActivity {
-    Button btncuento1, btncuento2;
+    ImageButton btncuento1, btncuento2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
